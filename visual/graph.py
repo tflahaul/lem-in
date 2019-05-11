c1d51344e52b30be1a12761f2d27842f83faf756
athenais <athenaisbrunet@gmail.com>
2019-05-12 01:47:36 +0200

    large graphs handle w/ sfdp

--- visual/graph.py
+++ visual/graph.py
@@ -2,37 +2,41 @@ import networkx as nx
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
-def get_options(G, vertices):
+def get_options(G, vertices, ax):
     options = {
-        'pos': nx.get_node_attributes(G, 'pos') if vertices < 50 else nx.random_layout(G),
-        'node_size': 500 if vertices < 50 else 10,
+        'pos': nx.nx_pydot.pydot_layout(G, prog='sfdp') if vertices > 100
+           else nx.get_node_attributes(G, 'pos'),
+        'node_size': 500 if vertices < 50 
+            else 10,
         'alpha': 0.60,
-        'width': 10.0 if vertices < 50 else 1.0,
+        'width': 10.0 if vertices < 50
+            else 1.0,
+        'ax': ax,
     }
     return (options)
 
 def graph(G, paths, data):
     shrtlen = len(paths[0])
     frames = shrtlen + len(paths[len(paths) - 1])
-    options = get_options(G, data['vertices']) 
 
     # set subplots
     fig, ax = plt.subplots(figsize=(6, 4))
+    options = get_options(G, data['vertices'], ax)
 
     def update(i):
         ax.clear()
         plt.gca().invert_yaxis()
 
         # draw background graph
-        nx.draw_networkx_edges(G, **options, ax=ax, edge_color="#0D7D84")
-        nx.draw_networkx_nodes(G, **options, ax=ax, node_color="#07B2B4")
-
+        nx.draw_networkx_edges(G, **options, edge_color="#0D7D84")
+        nx.draw_networkx_nodes(G, **options, node_color="#07B2B4")
+        
         # draw shortest path
         if (i and i <= shrtlen):
             nodelist = paths[0][(i - i):(i + 1)]
             edgelist = [nodelist[k:k+2] for k in range(len(nodelist) - 1)]
-            nx.draw_networkx_nodes(G, **options, nodelist=nodelist, node_color='yellow', ax=ax)
-            nx.draw_networkx_edges(G, **options, edgelist=edgelist, edge_color='yellow', ax=ax)
+            nx.draw_networkx_nodes(G, **options, nodelist=nodelist, node_color='yellow')
+            nx.draw_networkx_edges(G, **options, edgelist=edgelist, edge_color='yellow')
             ax.set_title("SHORTEST PATH")
 
         # draw any other needed paths 
@@ -41,8 +45,8 @@ def graph(G, paths, data):
             for n in range(len(paths) - 1):
                 nodelist = paths[n + 1][(idx - idx):(idx + 1)]
                 edgelist = [nodelist[k:k+2] for k in range(len(nodelist) - 1)]
-                nx.draw_networkx_nodes(G, **options, nodelist=nodelist, node_color='orange', ax=ax)
-                nx.draw_networkx_edges(G, **options, edgelist=edgelist, edge_color='orange', ax=ax)
+                nx.draw_networkx_nodes(G, **options, nodelist=nodelist, node_color='orange')
+                nx.draw_networkx_edges(G, **options, edgelist=edgelist, edge_color='orange')
             ax.set_title("ALTERNATE PATHS") 
         ax.set_facecolor("#94DAD5")
 
